0d99a5493c1232402ae209b5f68644eb09cf9d53
CanisExDaemonumUrbe <d.gelimyanov@yandex.ru>
2025-09-11 03:12:22 +0500

    Squashed 'packages/mailer-api/' content from commit ac3f024
    git-subtree-dir: packages/mailer-api
    git-subtree-split: ac3f024859c96b35c165ad75e806938fac88d233

---
+++ source/local_modules/packager/Packager.js
@@ -0,0 +1,89 @@
+class Packager {
+
+    async packObjectAsync(status, data = null){
+
+        let pack = {
+            status: "undefined",
+            data: null
+        };
+
+        try{
+
+            pack['status'] = status;
+            pack['data'] = data;
+
+        } catch (err) {
+
+            pack['status'] = "error";
+            pack['data'] = err.message;
+
+        } finally {
+
+            return pack;
+        }
+
+
+    }
+
+    async unpackObjectAsync(pack){
+
+        function convertData(data){
+
+            let result;
+
+            if(data){
+
+                try{
+
+                    if(data.length === `undefined` || data.length == 0 || JSON.stringify(data) == `{}`) {
+    
+                        result = null;
+    
+                    } else {
+    
+                        result = data;
+                    }
+    
+                } catch {
+    
+                    result = null;
+                }
+
+            } else {
+
+                result = data;
+            }
+
+            return result;
+        }
+
+        let status = null;
+        let data = null;
+
+        try{
+
+            if(pack[`status`] == `error`){
+
+                status = pack[`data`];
+
+            } else {
+
+                status = pack[`status`];
+                data = convertData(pack[`data`]);
+            }
+
+        } catch (err) {
+
+            unpackedObj[0] = `unpack_error`;
+            unpackedObj[1] = err.message;
+
+        } finally {
+
+            return [status, data];
+        }
+
+    }
+
+}
+
+module.exports = Packager
